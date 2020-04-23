9ad4c5d550f24dc2dcd5d7211e8e4a875a0e1f59
Camilo Muñoz <camilo.munozmoreno@cencosud.cl>
2020-04-22 22:20:29 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo como estas Este es um prgrmaa de prueba
+          Estas en mi mundo.!!!!
           
         </p>
         <a
